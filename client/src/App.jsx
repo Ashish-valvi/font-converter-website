ca1944a87c2c068ce0c42b44c476518f88111c47
Ashish-valvi <ashishvalvi.nvp@gmail.com>
2026-05-05 14:26:44 +0530

    Add compiled dist folder

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:5000/convert", {
+      const res = await fetch("/api/convert", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
